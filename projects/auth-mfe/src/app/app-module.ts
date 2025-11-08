ab5b75c32b4370c2826662e90893f7e5c449d1c7
Nexeum <54515356+Nexeum@users.noreply.github.com>
2025-11-08 18:28:31 -0500

    Remove AppRoutingModule import from AppModule
    - AppModule was importing non-existent AppRoutingModule after router cleanup
    - Remove import and usage to fix compilation error in auth-mfe tests
    - AppModule is legacy file, auth-mfe now uses standalone bootstrapApplication

--- projects/auth-mfe/src/app/app-module.ts
+++ projects/auth-mfe/src/app/app-module.ts
@@ -2,14 +2,11 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { InfrastructureModule } from 'infrastructure';
 
-import { AppRoutingModule } from './app-routing-module';
-
 @NgModule({
   declarations: [
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     InfrastructureModule.forRoot()
   ],
   providers: [
